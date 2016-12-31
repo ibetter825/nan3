d927733dd62c51f788c2f7a8c029df6cdefe2d1f
ibetter825 <y825y@qq.com>
2016-12-31 23:21:44 +0800

    layer 修改 01

--- src/page/layer.vue
+++ src/page/layer.vue
@@ -3,7 +3,19 @@
       <div>
         <button type="button" @click="msg">消息框</button>
       </div>
-      <layer-mobile v-if="layer.show" v-long.prevent = "{ methods: tap }" v-left.prevent = "{ methods: left }"></layer-mobile>
+      <br/>
+      <div>
+        <button type="button" @click="tip">提示框</button>
+      </div>
+      <br/>
+      <div>
+        <button type="button" @click="bottom">底部提示</button>
+      </div>
+      <br/>
+      <div>
+        <button type="button" @click="loading">加载层</button>
+      </div>
+      <layer-mobile v-if="layer.show"></layer-mobile>
     </div>
 </template>
 <script>
@@ -30,18 +42,40 @@
                 alert('left');
             },
             msg: function() {
-                if (!this.layer.show) this.layer.show = true //设置为true一步加载组件
-
                 this.$store.dispatch('layer_show', true)
                     //初始化弹出层配置
                 this.$store.dispatch('layer_init', {
-                    type: 2,
-                    shade: false
+                    title: '消息',
+                    content: '这是弹出的消息'
+                });
+            },
+            tip: function() {
+                this.$store.dispatch('layer_show', true)
+                    //初始化弹出层配置
+                this.$store.dispatch('layer_init', {
+                    skin: 'msg',
+                    shade: false,
+                    content: '这是弹出的消息'
+                });
+            },
+            loading: function() {
+                this.$store.dispatch('layer_show', true)
+                    //初始化弹出层配置
+                this.$store.dispatch('layer_init', {
+                    type: 2
+                });
+            },
+            bottom: function() {
+                this.$store.dispatch('layer_show', true)
+                    //初始化弹出层配置
+                this.$store.dispatch('layer_init', {
+                    content: '一个没有任何按钮的底部提示',
+                    skin: 'footer'
                 });
             }
         },
         created: function() {
-
+            this.layer.show = true //设置为true异步加载组件
         }
     }
 </script>
