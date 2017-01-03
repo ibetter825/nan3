3ba01fba5f3c45c57af397573a681f0f6ab610eb
ibetter825 <y825y@qq.com>
2017-01-03 15:22:43 +0800

    vue-resource

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 // 引用路由
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
 import Polyfill from 'babel-polyfill'
 
