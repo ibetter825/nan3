a6e2ca147ae6dfeca46d98cc44342b94856136ed
ibetter825 <y825y@qq.com>
2017-01-05 19:10:14 +0800

    detail append 03

--- src/components/n3/footer.vue
+++ src/components/n3/footer.vue
@@ -1,37 +1,76 @@
 <template>
-        <footer class="footer pd10">
-            <ul>
-                <li>
-                    <a class="selected" href="">
-                        <p><i class="iconfont icon-shouye"></i></p>
-                        <p><span>首页</span></p>
-                    </a>
-                </li>
-                <li>
-                    <a href="view/goods/type.html">
-                        <p><i class="iconfont icon-fenlei"></i></p>
-                        <p><span>分类</span></p>
-                    </a>
-                </li>
-                <li id="cart">
-                    <a href="view/cart.html">
-                        <p><i class="iconfont icon-gouwuche"></i></p>
-                        <p><span>购物车</span></p>
-                    </a>
-                </li>
-                <li>
-                    <a href="view/user/zone.html">
-                        <p><i class="iconfont icon-gerenzhongxin"></i></p>
-                        <p><span>个人中心</span></p>
-                    </a>
-                </li>
-            </ul>
-        </footer>
-    </template>
+    <footer :class="['footer', type === 0 ? 'pd10' : '']">
+        <ul v-if="type === 0">
+            <li>
+                <a class="selected" href="">
+                    <p><i class="iconfont icon-shouye"></i></p>
+                    <p><span>首页</span></p>
+                </a>
+            </li>
+            <li>
+                <a href="view/goods/type.html">
+                    <p><i class="iconfont icon-fenlei"></i></p>
+                    <p><span>分类</span></p>
+                </a>
+            </li>
+            <li id="cart">
+                <a href="view/cart.html">
+                    <p><i class="iconfont icon-gouwuche"></i></p>
+                    <p><span>购物车</span></p>
+                </a>
+            </li>
+            <li>
+                <a href="view/user/zone.html">
+                    <p><i class="iconfont icon-gerenzhongxin"></i></p>
+                    <p><span>个人中心</span></p>
+                </a>
+            </li>
+        </ul>
+        <ul v-if="type === 1">
+            <li>
+                <a class="selected" href="../../index.html">
+                    <p><i class="iconfont icon-shouye"></i></p>
+                    <p><span>首页</span></p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <p><i class="iconfont icon-gouwuche"></i></p>
+                    <p><span>购物车</span></p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <p><i class="iconfont icon-jushoucang"></i></p>
+                    <p><span>收藏</span></p>
+                </a>
+            </li>
+            <li class="bg-orange">
+                <a href="" class="white">
+                    加入购物车
+                </a>
+            </li>
+            <li class="bg-red">
+                <a href="" class="white">
+                    立即购买
+                </a>
+            </li>
+        </ul>
+    </footer>
+</template>
 <script>
     module.exports = {
+        props: ['prop'],
         data() {
             return {}
+        },
+        computed: {
+            type() {
+                let t = 0
+                if (this.prop && this.prop.type)
+                    t = this.prop.type
+                return t
+            }
         }
     }
 </script>
--- src/page/goods/detail.vue
+++ src/page/goods/detail.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="detail">
+    <div class="detail goods">
         <n3-top>
             <li class="back" slot="left"><i class="iconfont icon-xiangzuojiantou f18"></i></li>
             <li slot="right">
@@ -7,16 +7,46 @@
                     <i class="iconfont icon-others"></i>
                 </a>
             </li>
-            <li class="title" slot="center">圣诞树大户大叔的宿舍的</li>
+            <li class="title" slot="center">{{title}}</li>
         </n3-top>
         <section class="main">
             <n3-slider :prop="slider"></n3-slider>
+            <section class="header pd10-20 rtive">
+                <h1 class="mg0 f18">
+                    华秧安岳黄柠檬新鲜水果二三级皮薄柠檬切片泡茶榨汁酸味十足4斤
+                </h1>
+                <div class="share abte">
+                    <p><i class="iconfont icon-fenxiang f24"></i></p>
+                    <p class="f12">分享</p>
+                </div>
+                <div class="price">
+                    <p class="f24"><i class="f16 orange">￥</i><span class="orange">{{price}}</span></p>
+                    <p class="gray f12"><i>价格</i><span class="through">￥{{marketPrice}}</span></p>
+                </div>
+                <ul class="info gray f12">
+                    <li class="fl">快递:￥0</li>
+                    <li class="fl">月销量:100</li>
+                    <li class="fr">遂宁</li>
+                    <div class="clear"></div>
+                </ul>
+            </section>
         </section>
+        <div class="divider divider5"></div>
+        <n3-footer :prop="footer"></n3-footer>
     </div>
 </template>
-
 <script>
     module.exports = {
+        data() {
+            return {
+                title: '圣诞树大户大叔的宿舍的',
+                price: 250,
+                marketPrice: 1250,
+                footer: {
+                    type: 1
+                }
+            }
+        },
         computed: {
             slider() { //传递给slider组件的数据，如果url === null, 则直接填充数据data，反之则通过url地址以及param去请求后台获取
                 return {
