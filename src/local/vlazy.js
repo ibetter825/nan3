7b76dd4f4f11ed789968c0719d9a3400c02cd199
ibetter825 <y825y@qq.com>
2017-01-05 12:37:41 +0800

    v-lazy append 06

--- src/local/vlazy.js
+++ src/local/vlazy.js
@@ -3,18 +3,31 @@ import $util from './util'
 // dom.dispatchEvent(evt)
 const TAG_NAME_IMG = 'IMG'
 const LOADING_IMG = '/static/images/loader0.gif'
+const ERROR_IMG = '/static/images/load_error.png'
 const CLIENT_HEIGHT = document.documentElement.clientHeight // window.screen.availHeight
-let LAST_SCROLL_TOP = 0 //记录上一次滚动的scrollTop 用于判断滚动方向
+let LAST_SCROLL_TOP = 0 // 记录上一次滚动的scrollTop 用于判断滚动方向
 
 // listen事件执行的方法
 function listen (e) {
   let el = e.target
   let img = el
-  if(el.tagName !== TAG_NAME_IMG)
-      img = el.getElementsByTagName('img')[0]
+  if (el.tagName !== TAG_NAME_IMG)
+    img = el.getElementsByTagName('img')[0]
   img.src = el.getAttribute('data-url')
-  el.setAttribute('data-lazy', 'loaded') // 给改变元素属性，用于查询遍历
-  el.removeAttribute('data-url')
+
+  // 加载成功
+  addListener(img, 'load', function () {
+    el.setAttribute('data-lazy', 'loaded') // 给改变元素属性，用于查询遍历
+    el.removeAttribute('data-url')
+  }, false)
+
+  // 加载失败
+  addListener(img, 'error', function () {
+    el.setAttribute('data-lazy', 'error') // 给改变元素属性，用于查询遍历
+    el.style.background = 'url(' + ERROR_IMG + ') no-repeat center'
+    img.style.visibility = 'hidden'
+  }, false)
+
   // 移除事件监听
   removeListener(el, 'listen', listen, false)
 }
@@ -30,39 +43,42 @@ window.addEventListener('scroll', function () {
   let lazies = document.querySelectorAll('[data-lazy=unload]')
   let el = null, etop = 0, ebottom = 0, height = 0
   for (let i = 0, l = lazies.length; i < l; i++) {
-      el = lazies[i]
+    el = lazies[i]
     etop = el.getBoundingClientRect().top
     ebottom = el.getBoundingClientRect().bottom
-    height = ebottom - etop//元素的高度
-
-    if ((isDown && etop >= 0 && etop <= CLIENT_HEIGHT) || (!isDown && ebottom >= -10)) {
+    height = ebottom - etop // 元素的高度
+    if (isInView(el)) {
       lazies[i].dispatchEvent(evt); // 触发事件
-      return
+    }else {
+      if ((isDown && etop >= 0 && etop <= CLIENT_HEIGHT) || (!isDown && ebottom >= -10)) {
+        lazies[i].dispatchEvent(evt); // 触发事件
+        return
+      }
     }
   }
 }, false)
 /**
  * 添加事件监听
  */
-function addListener(el, type, fn, capture){
+function addListener (el, type, fn, capture) {
   if (window.addEventListener)
-      el.addEventListener(type, fn, capture)
-    else if (window.attachEvent)
-      el.attachEvent('on'+type, fn)
+    el.addEventListener(type, fn, capture)
+  else if (window.attachEvent)
+    el.attachEvent('on' + type, fn)
 }
 /**
  * 移除监听事件
  */
-function removeListener(el, type, fn, capture){
+function removeListener (el, type, fn, capture) {
   if (window.removeEventListener)
     el.removeEventListener(type, fn, capture)
   else if (window.detachEvent)
-    el.detachEvent('on'+type, fn)
+    el.detachEvent('on' + type, fn)
 }
 /**
  * 是否向下滚动
  */
-function isScrollDown(){
+function isScrollDown () {
   let top = $util.scrollTop()
   let isDown = top > LAST_SCROLL_TOP ? true : false
   LAST_SCROLL_TOP = top
@@ -71,24 +87,24 @@ function isScrollDown(){
 /**
  * 判断是否在页面内
  */
-function isInView(el){
-    let etop = el.getBoundingClientRect().top
-    let ebottom = el.getBoundingClientRect().bottom
-    return ebottom < 0 ? false : CLIENT_HEIGHT - etop > 0
+function isInView (el) {
+  let etop = el.getBoundingClientRect().top
+  let ebottom = el.getBoundingClientRect().bottom
+  return ebottom < 0 ? false : CLIENT_HEIGHT - etop > 0
 }
 export default {
   bind: function (el, binding, vnode) {
-    //指定为container时需要
+    // 指定为container时需要
     el.setAttribute('data-lazy', 'unload') // 给元素添加属性，用于查询遍历
     el.setAttribute('data-url', binding.value)
-    if(el.tagName !== TAG_NAME_IMG)
-      el.getElementsByTagName('img')[0].src = LOADING_IMG
+    if (el.tagName !== TAG_NAME_IMG)
+      el.style.background = 'url(' + LOADING_IMG + ') no-repeat center'
     else
       el.src = LOADING_IMG
     // 监听listen事件
     addListener(el, 'listen', listen, false)
   },
-  inserted: function(el){
-      if(isInView(el)) el.dispatchEvent(evt); // 触发事件
+  inserted: function (el) {
+    if (isInView(el)) el.dispatchEvent(evt); // 触发事件
   }
 }
--- src/page/goods/detail.vue
+++ src/page/goods/detail.vue
@@ -4,18 +4,18 @@
         <n3-advert :prop="advert[0]"></n3-advert>
         <n3-advert :prop="advert[1]"></n3-advert>
         <n3-advert :prop="advert[2]"></n3-advert>
-        <n3-advert :prop="advert[0]"></n3-advert>
-        <n3-advert :prop="advert[1]"></n3-advert>
-        <n3-advert :prop="advert[2]"></n3-advert>
-        <n3-advert :prop="advert[0]"></n3-advert>
-        <n3-advert :prop="advert[1]"></n3-advert>
-        <n3-advert :prop="advert[2]"></n3-advert>
-        <n3-advert :prop="advert[0]"></n3-advert>
-        <n3-advert :prop="advert[1]"></n3-advert>
-        <n3-advert :prop="advert[2]"></n3-advert>
-        <n3-advert :prop="advert[0]"></n3-advert>
-        <n3-advert :prop="advert[1]"></n3-advert>
-        <n3-advert :prop="advert[2]"></n3-advert>
+        <n3-advert :prop="advert[3]"></n3-advert>
+        <n3-advert :prop="advert[4]"></n3-advert>
+        <n3-advert :prop="advert[5]"></n3-advert>
+        <n3-advert :prop="advert[6]"></n3-advert>
+        <n3-advert :prop="advert[7]"></n3-advert>
+        <n3-advert :prop="advert[8]"></n3-advert>
+        <n3-advert :prop="advert[9]"></n3-advert>
+        <n3-advert :prop="advert[10]"></n3-advert>
+        <n3-advert :prop="advert[11]"></n3-advert>
+        <n3-advert :prop="advert[12]"></n3-advert>
+        <n3-advert :prop="advert[13]"></n3-advert>
+        <n3-advert :prop="advert[14]"></n3-advert>
     </div>    
 </template>
 
@@ -25,15 +25,63 @@ computed: {
             advert() {
                 return [{
                     url: '#',
-                    img: '/static/images/slider.gif',
+                    img: 'http://www.3lian.com/d/file/201701/03/0f2be5699c075392390d21f2478e8b45.jpgs',
                     text: '卖水果了'
                 }, {
                     url: '#',
-                    img: '/static/images/slider.gif',
+                    img: 'http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1308/17/c2/24561081_1376699839025_800x600.jpg',
                     text: '卖酱油了'
                 }, {
                     url: '#',
-                    img: '/static/images/slider.gif',
+                    img: 'http://img1.imgtn.bdimg.com/it/u=1089582262,166446285&fm=23&gp=0.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://pic47.nipic.com/20140902/12918166_193551217000_2.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://www.3lian.com/d/file/201701/03/dc63d56d6ffe30dd694a8c82837787e7.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://img1.imgtn.bdimg.com/it/u=1743887942,3562408053&fm=23&gp=0.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://desk.fd.zol-img.com.cn/t_s960x600c5/g2/M00/08/04/Cg-4WlUsfneIaXjtAAw3U90BfkIAABhDwJlBbUADDdr624.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://tupian.enterdesk.com/2013/lxy/12/27/2/9.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://www.3lian.com/d/file/201701/03/1fa013d428b3d8341b868353b71ad0a0.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://www.3lian.com/d/file/201701/03/2c87d996fd182fb0de9b40d17a6c90e8.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://www.3lian.com/d/file/201701/03/1aeb24b4b0fb9952f973461ebecf0bd6.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://tupian.enterdesk.com/2013/mxy/07/0715/1/7.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://www.3lian.com/d/file/201701/03/79af5b7531e3505f15c4a0afc0d883d9.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://img3.imgtn.bdimg.com/it/u=1207302204,1095596055&fm=23&gp=0.jpg',
+                    text: '卖冬瓜了'
+                }, {
+                    url: '#',
+                    img: 'http://www.3lian.com/d/file/201701/03/8266dd7fdb5853fa8304ae86cbb3134a.jpg',
                     text: '卖冬瓜了'
                 }]
             }
