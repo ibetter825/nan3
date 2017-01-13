a50417e62a82d2099455310a87c65e16624f6f4d
ibetter825 <y825y@qq.com>
2017-01-13 14:03:09 +0800

    confirm append 02

--- src/page/order/confirm.vue
+++ src/page/order/confirm.vue
@@ -1,13 +1,30 @@
 <template>
-    <div class="cart">
+    <div class="confirm">
         <n3-top>
             <li class="back" @click="back" slot="left"><i class="iconfont icon-xiangzuojiantou f18"></i></li>
             <li slot="right"></li>
             <li slot="center">确认订单</li>
         </n3-top>
         <section class="main">
-            <p>收货地址：如不设置则需要补充</p>
-            <p>订单商品列表</p>
+            <ul class="rec-info pd0-10">
+                <li>收货人：<span>XXX</span> <span>18502820654</span></li>
+                <li class="addr">
+                    <div class="fl"><i class="iconfont icon-shouhuodizhi"></i></div>
+                    <div class="fr text-right"><i class="iconfont icon-xiangyoujiantou"></i></div>
+                    <div class="f12">收货地址：<span>四川省成都市金牛区交大路222号2栋2单元202号</span></div>
+                </li>
+                <li class="orange">(补充说明类</li>
+            </ul>
+            <div class="divider"></div>
+            <ul class="order-list pd5-10">
+                <li>
+                    <div>shangbuqishuiguodian</div>
+                    <div>shangpingxiangqing</div>
+                    <div>xxx</div>
+                    <div>xxx</div>
+                </li>
+                <li>2</li>
+            </ul>
         </section>
     </div>
 </template>
